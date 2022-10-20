b8d1f059d0611f8fc493af3c5673b9af337154ae
makarworld <zippo656476@gmail.com>
2022-10-21 01:47:14 +0300

    create tests for client.py

--- tests/tests_client.py
+++ tests/tests_client.py
@@ -1,19 +1,22 @@
 import pytest
 from dotenv import dotenv_values
-from litewax import Client, Anchor, WCW
-from litewax.exceptions import AuthNotFound, CookiesExpired
-
 ENV = dotenv_values(".env")
 
 PVT_KEY = ENV.get("PVT_KEY")
 COOKIE = ENV.get("COOKIE")
+
+
+
+
+from litewax import Client, Anchor, WCW
+from litewax.exceptions import AuthNotFound, CookiesExpired
 
 def test_create_anchor_good():
     client = Client(private_key=PVT_KEY)
     assert isinstance(client, Client) 
 
     assert client.name == "atonicmaiket"
-    assert client.node == "https://wax.greymass.com"
+    assert client.waxclient.node == "https://wax.greymass.com"
     assert client.type == "private_key"
     assert isinstance(client.waxclient, Anchor)
 
@@ -22,7 +25,7 @@ def test_create_wcw_good():
     assert isinstance(client, Client) 
 
     assert client.name == "zknmi.wam"
-    assert client.node == "https://wax.greymass.com"
+    assert client.waxclient.utils.node == "https://wax.greymass.com"
     assert client.type == "cookie"
     assert isinstance(client.waxclient, WCW)
 
