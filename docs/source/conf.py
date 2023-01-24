2458f88361563a11f2c6b4f7d744b71a4c565a9c
makarworld <zippo656476@gmail.com>
2023-01-24 12:59:14 +0300

    depricate docs. will use wiki instead docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Litewax'
+copyright = '2022, abuztrade'
+author = 'abuztrade'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.1.7'
+version = '0.1.7'
 
 # -- General configuration
 
